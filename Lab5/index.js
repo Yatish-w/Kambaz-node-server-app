d6cc1f54df269d19fd4678abae5464b3b181a8cb
yatish79 <wutla.y@northeastern.edu>
2025-04-04 20:39:13 -0400

    Update CORS configuration to fix sign-in issues

--- Lab5/index.js
+++ Lab5/index.js
@@ -9,8 +9,13 @@ export default function Lab5(app) {
         credentials: true,
         origin: [
             'http://localhost:5173',
-            'https://a5--kambaz-react-web-app-yw.netlify.app'
-        ]
+            'https://a5--kambaz-react-web-app-yw.netlify.app',
+            'https://a5--kambaz-react-web-app-yw.netlify.app/#/LandingPage'
+        ],
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        exposedHeaders: ['Content-Range', 'X-Content-Range'],
+        optionsSuccessStatus: 200
     }));
     app.get("/lab5/welcome", (req, res) => {
         res.send("Welcome to Lab 5");
