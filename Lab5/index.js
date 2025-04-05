18fb279d8fc9a5fa95e557a0598f88ffed0a96fb
yatish79 <wutla.y@northeastern.edu>
2025-04-04 20:33:14 -0400

    Update CORS configuration for Netlify domain

--- Lab5/index.js
+++ Lab5/index.js
@@ -5,7 +5,13 @@ import WorkingWithObjects from "./WorkingWithObjects.js";
 import cors from "cors";
 
 export default function Lab5(app) {
-    app.use(cors());
+    app.use(cors({
+        credentials: true,
+        origin: [
+            'http://localhost:5173',
+            'https://a5--kambaz-react-web-app-yw.netlify.app'
+        ]
+    }));
     app.get("/lab5/welcome", (req, res) => {
         res.send("Welcome to Lab 5");
     });
